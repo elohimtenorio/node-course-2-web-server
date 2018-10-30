9d90b5a196f3c9479f1caac5fc6bd6cad0804148
Elohim Tenorio <elohim@Elohims-MacBook-Pro.local>
2018-10-30 02:01:28 -0300

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -17,9 +19,9 @@ app.use((req, res, next)=>{
   next();
 });
 
-app.use((req,res,next)=>{
-  res.render('maintenance.hbs');
-});
+//app.use((req,res,next)=>{
+  //res.render('maintenance.hbs');
+//});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -50,6 +52,6 @@ app.get('/bad',(req,res)=>{
     errorMessage:'Unable to handle request'
   })
 });
-app.listen(3000, ()=>{
-  console.log('Server is up in port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is up in port ${port}`);
 });
